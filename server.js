9760b01a112921955ab398df317855b1c60bbd76
Renan Ventura <renan.al.ventura@gmail.com.br>
2024-12-16 11:36:59 -0300

    Atualização do Banco de dados

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
 
+
+//################################################### Rota Get ######################################################################################################
+
+
 app.get('/Ferramentas',  async (req, res) => {
     
     const listaFerramenta = await prisma.ferramentas.findMany()
@@ -17,6 +21,33 @@ app.get('/Ferramentas',  async (req, res) => {
     res.status(200).json(listaFerramenta)
 
 })
+
+app.get('/FerramentaHistorico',  async (req, res) => {
+    
+    const listaFerramenta = await prisma.ferramentaHistorico.findMany()
+
+    res.status(200).json(listaFerramenta)
+
+})
+
+app.get('/Empresa',  async (req, res) => {
+    
+    const holding = await prisma.empresa.findMany()
+
+    res.status(200).json(holding)
+
+})
+
+app.get('/CentroCusto',  async (req, res) => {
+    
+    const Classe = await prisma.centroCusto.findMany()
+
+    res.status(200).json(Classe)
+
+})
+
+//################################################### Rota Post ######################################################################################################
+
 
 app.post('/Ferramentas', async (req, res) => {
 
@@ -41,6 +72,57 @@ app.post('/Ferramentas', async (req, res) => {
 
     res.status(201).json(req.body)
 })
+
+app.post('/FerramentaHistorico', async (req, res) => {
+
+    await prisma.ferramentaHistorico.create({
+        data: {
+            Nome: req.body.Nome,             
+            Valor: req.body.Valor,           
+            Patrimonio: req.body.Patrimonio,        
+            Observacao: req.body.Observacao,       
+            Empresa: req.body.Empresa,           
+            CentroDeCusto: req.body.CentroDeCusto,     
+            NomeDeResponsavel: req.body.NomeDeResponsavel, 
+            TipoDeCadastro: req.body.TipoDeCadastro,    
+            Status:req.body.Status,
+            StatusDelete:req.body.StatusDelete,
+            ObsEmprestado:req.body.ObsEmprestado,
+            ResponsavelEmprestado:req.body.ResponsavelEmprestado,
+            DataEmprestado:req.body.DataEmprestado,
+            DataDevolvida: req.body.DataDevolvida,
+            DateAlterado: req.body.DateAlterado
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.post('/Empresa', async (req, res) => {
+
+    await prisma.empresa.create({
+        data: {
+            Empresa: req.body.Empresa,
+            StatusDelete: req.body.StatusDelete
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.post('/CentroCusto', async (req, res) => {
+
+    await prisma.centroCusto.create({
+        data: {
+            CentroCusto: req.body.CentroCusto,
+            StatusDelete: req.body.StatusDelete
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+//################################################### Rota Put ######################################################################################################
 
 app.put('/Ferramentas/:id', async (req, res) => {
 
@@ -69,9 +151,82 @@ app.put('/Ferramentas/:id', async (req, res) => {
     res.status(201).json(req.body)
 })
 
+
+app.put('/FerramentaHistorico/:id', async (req, res) => {
+
+    await prisma.ferramentaHistorico.update({
+        where: {
+            id:req.params.id
+        },
+        data: {
+            Nome: req.body.Nome,             
+            Valor: req.body.Valor,           
+            Patrimonio: req.body.Patrimonio,        
+            Observacao: req.body.Observacao,       
+            Empresa: req.body.Empresa,           
+            CentroDeCusto: req.body.CentroDeCusto,     
+            NomeDeResponsavel: req.body.NomeDeResponsavel, 
+            TipoDeCadastro: req.body.TipoDeCadastro,    
+            Status:req.body.Status,
+            StatusDelete:req.body.StatusDelete,
+            ObsEmprestado:req.body.ObsEmprestado,
+            ResponsavelEmprestado:req.body.ResponsavelEmprestado,
+            DataEmprestado:req.body.DataEmprestado,
+            DataDevolvida: req.body.DataDevolvida,
+            DateAlterado: req.body.DateAlterado
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.put('/Empresa/:id', async (req, res) => {
+
+    await prisma.empresa.update({
+        where: {
+            id:req.body.id
+        },
+        data: {
+            Empresa: req.body.Empresa,
+            StatusDelete: req.body.StatusDelete
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.put('/CentroCusto/:id', async (req, res) => {
+
+    await prisma.centroCusto.update({
+        where:{
+            id:req.body.id
+        },
+        data: {
+            CentroCusto: req.body.CentroCusto,
+            StatusDelete: req.body.StatusDelete
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+//################################################### Rota Delete ######################################################################################################
+
+
 app.delete('/Ferramentas/:id', async (req, res) => {
 
     await prisma.ferramentas.delete({
+        where: {
+            id:req.params.id
+        },
+    })
+    res.status(201).json({message: "Usuario Deletado com Sucesso!"})
+})
+
+
+app.delete('/FerramentaHistorico/:id', async (req, res) => {
+
+    await prisma.ferramentaHistorico.delete({
         where: {
             id:req.params.id
         },
